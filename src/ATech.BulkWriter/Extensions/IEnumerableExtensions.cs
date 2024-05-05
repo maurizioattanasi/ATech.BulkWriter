4287f1e26312b79427e74155912ac652828d8c7d
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2024-05-05 11:06:45 +0200

    Added extension

---
+++ src/ATech.BulkWriter/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+using System.Data;
+
+namespace ATech.BulkWriter;
+
+public static class IEnumerableExtensions
+{
+    public static DataTable ToDataTable<T>(this IEnumerable<T> source)
+    {
+        return new ObjectShredder<T>().Shred(source, null, null);
+    }
+
+    public static DataTable ToDataTable<T>(this IEnumerable<T> source,
+                                                DataTable table, LoadOption? options)
+    {
+        return new ObjectShredder<T>().Shred(source, table, options);
+    }
+}
